e17851ff9ed09d505188e85560413c45f7000313
John Tigue <john@tigue.com>
2016-01-29 15:04:14 -0800

    turning farcap into a one-liner require() module.

---
+++ src/farcap/index.js
@@ -0,0 +1,7 @@
+// This file enables folks to pull in farcap via a oneliner: var func = require('./farcap');
+var geocoder = require('./CachingGeocoder.js');
+
+module.exports = {
+    version = '0.1.0';
+    
+}
