c33c3a213fe200f74a62127c8925b3b8c8748faf
John Tigue <john@tigue.com>
2016-02-02 12:35:53 -0800

    Making Farcap a Funcy Module.

--- src/farcap/index.js
+++ src/farcap/index.js
@@ -1,15 +1,27 @@
-/* This file enables folks to pull in farcap via a oneliner: var func = require('./farcap');
+/*
+ * This file enables pulling in farcap via a oneliner: 
+ *   var func = require('./farcap');
+ *
  * There are various ways to design interfaces in JavaScript:
  *   http://bites.goodeggs.com/posts/export-this/
  * 
- * For new code, the options are narrowed nowadays to ES6/ES2015 modules
- * ("ES2015": Really, Really? This year model mistake needed to be repeated once again?)
- * My personal style is documented on GitHub:
+ * For new code, the options nowadays should really be similar to ES6/ES2015 modules.
+ *
+ * My personal style (implemented here) I call Funcy Modules. That is documented on GitHub:
  *   https://github.com/JohnTigue/javascripting/tree/master/src/explorations/funcy_modules
  */   
-var geocoder = require('./CachingGeocoder.js');
+module.exports = (function(){
+  'use strict'; 
+  var geocoder = require('./CachingGeocoder.js');
 
-module.exports = {
-    version: '0.1.0';
-    geocoder: geocoder;
-}
+  var aFuncy = function FarcapGeocoder(){
+    //JFT-TODO: do I want to get into fluent style? Is this the factory which returns instances?	
+    };
+
+  aFuncy.version  = 0.1.0;
+  aFuncy.geocoder = geocoder;
+    
+  return aFuncy;
+  }());
+ 
+
